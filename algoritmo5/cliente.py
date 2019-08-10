b237afdd3ffe63496770fff6d998c511ee82050a
Chervinski <22059902+JoaoChervinski@users.noreply.github.com>
2019-08-10 14:49:11 -0300

    Add files via upload
    Algoritmo 5 (Diffie-Hellman mais evolução de chaves com hkdf)

---
+++ algoritmo5/cliente.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+import socket, hmac, hashlib, sys, math, random, hkdf, os, base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+TAM_PAYLOAD = 512
+
+def derivar_chave(chave):
+    ''' Deriva uma chave para ser usada com o algoritmo de criptografia fernet
+    a partir da chave gerada pelo processo de diffie-hellman '''
+    salt = b''
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=10000,
+    backend=default_backend())
+    chave = base64.urlsafe_b64encode(kdf.derive(chave))
+
+    return chave
+
+
+def gerar_hmac(chave, info):
+    h = hmac.new(chave, info, hashlib.sha256)
+    return h.hexdigest()
+
+
+def verificar_hmac(payload, chave):
+    payload = remover_padding(payload)
+    msg_hmac = payload[-64:] # Os últimos 64 caracteres do payload são o HMAC
+    dados = payload[:-64]
+    h = hmac.new(chave, dados.encode(), hashlib.sha256)
+    if h.hexdigest() == msg_hmac:
+        return 'OK'
+    else:
+        return 'NOK'
+
+
+def criptografar(mensagem, chave):
+    mecanismo = Fernet(chave)
+    return mecanismo.encrypt((mensagem).encode())
+
+
+def decodificar(mensagem, chave):
+    mecanismo = Fernet(chave)
+    mensagem = mecanismo.decrypt(mensagem.encode()).decode()
+    return mensagem
+
+
+def eh_primo(num):
+    ''' Retorna True se num for um número primo, do contrário retorna False '''
+    if num == 1:
+        return False # 1 não é primo
+
+    divisor_maximo = math.floor(math.sqrt(num))
+    for y in range(2, divisor_maximo + 1):
+        if num % y == 0: # Se a operação de módulo retornar 0, num é divisível por y
+            return False # Se num é divisível por y (y != 1 e y != num), num não é primo
+    return True
+
+
+def gerar_parametros():
+    ''' Gera os parâmetros iniciais 'p' e 'g' da troca de chaves de Diffie-Hellman
+        Descrição do processo: https://bit.ly/2YUHXSd'''
+    q = random.randint(100000, 1000000) # Seleciona um valor inicial aleatório
+    r = 1
+
+    while True:
+        if eh_primo(q): # Se o valor for primo, será usado
+            break
+        else:
+            q += 1 # Se não for primo, incrementa e testa novamente
+
+    while True:
+        p = q * r + 1
+        if eh_primo(p):
+            break
+        else:
+            r += 1
+
+    while True:
+        u = random.randint(1,10000)
+        g = (u ** ((p - 1)/q)) % p
+        if g == 1:
+            continue
+        else:
+            break
+
+    return int(p), int(g)
+
+
+def conectar(PORTA):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Instancia um socket
+        sock.connect((socket.gethostname(), PORTA))
+    except ConnectionError:
+        print("[+] Erro na conexão com o servidor.")
+        raise SystemExit
+    return sock
+
+
+def adicionar_padding(mensagem):
+    encoded_size = len(bytes(mensagem.encode()))
+    padding = (TAM_PAYLOAD - encoded_size + len(mensagem))
+    # Preenche o final da mensagem com espaços vazios
+    return mensagem.ljust(padding, ' ')
+
+
+def remover_padding(mensagem):
+    # Remove o preenchimento ao final da mensagem
+    return mensagem.decode().rstrip(' ')
+
+
+def diffie_hellman(p, g, sock):
+    mensagem = str(p) + '/' + str(g) # mensagem = valor p/valor g
+    mensagem = adicionar_padding(mensagem) # Preenche a mensagem até atingir o tamanho do payload
+    sock.send(bytes(mensagem, 'utf-8')) # Envia os parâmetros iniciais p e g para o servidor
+
+    A = random.randint(1, 10000)
+    a = g ** A % p
+
+    resposta = sock.recv(TAM_PAYLOAD)
+    resposta = remover_padding(resposta)
+    b = int(resposta) # Armazena o valor 'b' recebido do servidor
+
+    mensagem = str(a) # mensagem = valor a
+    mensagem = adicionar_padding(mensagem)
+    sock.send(bytes(mensagem, 'utf-8')) # Compartilha o valor gerado 'a' com o servidor
+
+    chave_compartilhada = str(b ** A % p)
+    print("[+] Chave compartilhada gerada: {}".format(chave_compartilhada))
+
+    # Deriva uma chave que possa ser usada pelo algoritmo de criptografia
+    chave_compartilhada = derivar_chave(bytes(chave_compartilhada, 'utf-8'))
+    print(f"[+] Chave derivada: {chave_compartilhada.decode()}")
+
+    k = hkdf.hkdf_extract(None, chave_compartilhada)
+
+    nova_chave = hkdf.hkdf_expand(k) # Expande a chave utilizando hkdf
+    nova_chave = derivar_chave(nova_chave)
+
+    mensagem = str(nova_chave)
+    mensagem = criptografar(mensagem, chave_compartilhada).decode()
+    mensagem += gerar_hmac(chave_compartilhada, mensagem.encode())
+    mensagem = adicionar_padding(mensagem)
+    sock.send(bytes(mensagem, 'utf-8'))
+
+    chave_compartilhada = nova_chave # Atualiza a chave para a nova chave gerada
+    print("[+] Nova chave gerada: {}".format(chave_compartilhada.decode()))
+
+    resposta = sock.recv(TAM_PAYLOAD)
+
+    if verificar_hmac(resposta, chave_compartilhada) == 'NOK':
+        print("[+] O HMAC da mensagem recebida não corresponde aos dados.")
+        raise SystemExit
+
+    resposta = remover_padding(resposta)
+    resposta = resposta[:-64] # Retira os últimos 64 bytes da mensagem (HMAC)
+    resposta = decodificar(resposta, chave_compartilhada)
+    print(resposta)
+
+
+def main(porta):
+    sock = conectar(porta)
+    p, g = gerar_parametros()
+    diffie_hellman(p, g, sock)
+    print("\n[+] Fim da conexão com o servidor.\n")
+    sock.close()
+
+
+if len(sys.argv) < 2:
+    print(f"[+] Utilização: {sys.argv[0].lstrip('./')} <porta>")
+    print("[+] <porta> : Porta da rede na qual o servidor está operando")
+    raise SystemExit
+
+try:
+    porta = int(sys.argv[1])
+except ValueError:
+    print("[+] Por favor utilize somente números inteiros para a porta.")
+    raise SystemExit
+
+main(porta)
---
+++ algoritmo5/servidor.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+import socket, hmac, hashlib, sys, math, random, hkdf, os, base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+from comandos import * # Importa os comandos PUT, PUT_ACK, GET, GET_ACK, e NOTIFY
+
+TAM_PAYLOAD = 512
+
+def derivar_chave(chave):
+    ''' Deriva uma chave para ser usada com o algoritmo de criptografia fernet
+    a partir da chave gerada pelo processo de diffie-hellman '''
+    salt = b''
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=10000,
+    backend=default_backend())
+    chave = base64.urlsafe_b64encode(kdf.derive(chave))
+
+    return chave
+
+
+def criptografar(mensagem, chave):
+    mecanismo = Fernet(chave)
+    return mecanismo.encrypt((mensagem).encode())
+
+
+def decodificar(mensagem, chave):
+    mecanismo = Fernet(chave)
+    mensagem = mecanismo.decrypt(mensagem.encode()).decode()
+    return mensagem
+
+
+def gerar_hmac(chave, info):
+    h = hmac.new(chave, info, hashlib.sha256)
+    return h.hexdigest()
+
+
+def verificar_hmac(payload, chave):
+    payload = remover_padding(payload)
+    msg_hmac = payload[-64:] # Os últimos 64 caracteres do payload são o HMAC
+    dados = payload[:-64]
+    h = hmac.new(chave, dados.encode(), hashlib.sha256)
+    if h.hexdigest() == msg_hmac:
+        return 'OK'
+    else:
+        return 'NOK'
+
+
+def adicionar_padding(mensagem):
+    encoded_size = len(bytes(mensagem.encode()))
+    padding = (TAM_PAYLOAD - encoded_size + len(mensagem))
+    # Preenche o final da mensagem com espaços vazios
+    return mensagem.ljust(padding, ' ')
+
+
+def remover_padding(mensagem):
+    # Remove o preenchimento ao final da mensagem
+    return mensagem.decode().rstrip(' ')
+
+
+def diffie_hellman(cliente):
+    mensagem = cliente.recv(TAM_PAYLOAD)
+    mensagem = remover_padding(mensagem)
+    p, g = mensagem.split('/') # Recupera os valores de p e g
+    p = int(p) # Converte a string recebida para inteiro
+    g = int(g)
+    B = random.randint(1,10000)
+    b = g ** B % p
+
+    mensagem = str(b)
+    mensagem = adicionar_padding(mensagem)
+    cliente.send(bytes(mensagem, 'utf-8'))
+
+    resposta = cliente.recv(TAM_PAYLOAD)
+    resposta = remover_padding(resposta)
+    a = int(resposta) # Armazena o valor 'b' recebido do servidor
+
+    chave_compartilhada = str(a ** B % p)
+    print("[+] Chave compartilhada gerada: {}".format(chave_compartilhada))
+
+    # Deriva uma chave que possa ser usada pelo algoritmo de criptografia
+    chave_compartilhada = derivar_chave(bytes(chave_compartilhada, 'utf-8'))
+    print(f"[+] Chave derivada: {chave_compartilhada.decode()}")
+
+    nova_chave = cliente.recv(TAM_PAYLOAD) # Recebe do cliente a nova chave gerada com hkdf
+
+    # Verifica a integridade dos dados usando HMAC
+    if verificar_hmac(nova_chave, chave_compartilhada) == 'NOK':
+        print("[+] O HMAC da mensagem recebida não corresponde aos dados.")
+        raise SystemExit
+
+    nova_chave = remover_padding(nova_chave)
+    nova_chave = nova_chave[:-64] # Retira os últimos 64 bytes da mensagem (HMAC)
+    chave_compartilhada = decodificar(nova_chave, chave_compartilhada)
+
+    chave_compartilhada = bytes(chave_compartilhada[2:-1], 'utf-8') # Converte a chave de str para bytes
+    print("[+] Nova chave gerada: {}".format(chave_compartilhada.decode()))
+
+    mensagem = "[+] Servidor: Recebi a nova chave."
+    mensagem = criptografar(mensagem, chave_compartilhada).decode()
+    mensagem += gerar_hmac(chave_compartilhada, mensagem.encode())
+    mensagem = adicionar_padding(mensagem)
+    cliente.send(bytes(mensagem, 'utf-8')) # Responde ao cliente dizendo que recebeu a nova chave
+
+
+def main(porta):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Instancia um socket
+    sock.bind((socket.gethostname(), porta)) # Define endereço e porta do socket
+    sock.listen() # Aguarda conexões no endereço especificado
+
+    cliente, endereco = sock.accept() # Aceita solicitações de conexão
+    print(f"[+] Conexão com {endereco} estabelecida.\n")
+    diffie_hellman(cliente)
+    print(f"\n[+] Fim da conexão com {endereco}.\n")
+    sock.close()
+
+
+if len(sys.argv) < 2:
+    print(f"[+] Utilização: {sys.argv[0].lstrip('./')} <porta>")
+    print("[+] <porta> : Porta da rede na qual o servidor irá operar")
+    raise SystemExit
+
+try:
+    porta = int(sys.argv[1])
+except ValueError:
+    print("[+] Por favor utilize somente números inteiros para a porta.")
+    raise SystemExit
+
+main(porta)
